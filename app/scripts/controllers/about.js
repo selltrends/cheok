eaa4f115cbb1aed4ee3727323440609113ae9020
Alex Wang <selltrends@gmail.com>
2015-10-29 17:31:52 +0800

    First Angularjs Web projects

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cheokApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cheokApp
+ */
+angular.module('cheokApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cheokApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cheokApp
+ */
+angular.module('cheokApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
